1cfe1621380a0e89335b826c64ab341c953d1458
IrvingJVR-MX <106118394+IrvingJVR-MX@users.noreply.github.com>
2022-10-17 13:51:46 -0500

    Initial config

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import LogIn from './src/pages/LogIn';
+
+export default function App() {
+  return (
+   <LogIn/>
+  );
+}
+
---
+++ src/pages/LogIn.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { View,ScrollView, StyleSheet, Text } from "react-native"
+
+export default function LogIn() {
+  return (
+   <Text>Hola</Text>
+   )
+}
